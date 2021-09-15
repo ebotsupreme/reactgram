0deed852f38668185830bdf37d5e930067171f2a
Eddie Jung <ejung716@gmail.com>
2021-09-15 16:24:43 -0700

    Add signup page and restructure folder tree.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import Login from "./components/Login/Login";
+import Login from "./components/Authentication/Login/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ForgotPassword from "./components/Login/ForgotPassword";
+import ForgotPassword from "./components/Authentication/ForgotPassword/ForgotPassword";
+import SignUp from "./components/Authentication/Signup/Signup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/forgotPassword" component={ForgotPassword} />
+          <Route exact path="/signUp" component={SignUp} />
           <Route path="/" component={Login} />
         </Switch>
       </Router>
