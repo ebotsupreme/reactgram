033142a7fa3d95719e139f6051619438683f42e8
Eddie Jung <ejung716@gmail.com>
2021-09-14 17:48:52 -0700

    Install materialUI. Outline divs for login page.

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="Login-container">
+      <div className="Login-login-container">
+        <div className="Login-logo-container"></div>
+        <div className="Login-input-top-container"></div>
+        <div className="Login-input-bottom-container"></div>
+      </div>
+      <div className="Login-signup-container"></div>
+    </div>
+  );
 };
 
 export default Login;
