56b7d983268a337af4af4e44de4f23819cd9d08c
Eddie Jung <ejung716@gmail.com>
2021-09-16 11:34:52 -0700

    Create firebase config.

---
+++ src/firebase/config.tsx
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage"; // storage for images
+import { getFirestore } from "firebase/firestore"; // database
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANlhNEdBOkobmYomY0J0YbXkl_jNzF7q0",
+  authDomain: "reactgram-1241e.firebaseapp.com",
+  projectId: "reactgram-1241e",
+  storageBucket: "reactgram-1241e.appspot.com",
+  messagingSenderId: "521870646125",
+  appId: "1:521870646125:web:9ac180b0b035e4f8e45864",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+const projectStorage = getStorage(firebaseApp);
+const projectFirestore = getFirestore();
+
+export { projectStorage, projectFirestore };
